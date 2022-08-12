8cac6e7c036868e549dcc22e89868cd7e352a704
Jorge.T.T <71452849+JorgeTTor@users.noreply.github.com>
2022-08-11 20:57:16 -0500

    [ADD] Mobile menu and toggle logic

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const menuHambIcon = document.querySelector('.menu-hamb')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHambIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
+  console.log('click')
+}
+
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive');
 }
