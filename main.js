90eece98f196f1f384e2f249b7acb446a08f6602
Jorge.T.T <71452849+JorgeTTor@users.noreply.github.com>
2022-08-12 19:59:54 -0500

    [ADD] shopping cart to navbar

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const menuEmail = document.querySelector('.navbar-email')
+const menuCartIcon = document.querySelector('.navbar-shopping-cart')
 const desktopMenu = document.querySelector('.desktop-menu')
 const menuHambIcon = document.querySelector('.menu-hamb')
 const mobileMenu = document.querySelector('.mobile-menu')
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHambIcon.addEventListener('click', toggleMobileMenu);
+menuCartIcon.addEventListener('click', toggleCartAside);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
@@ -13,5 +16,22 @@ function toggleDesktopMenu() {
 
 
 function toggleMobileMenu() {
+  const isAsideClosed = aside.classList.contains('inactive');
+  
+  if(!isAsideClosed) {
+    aside.classList.add('inactive');
+  }
   mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCartAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+  // aside.classList.toggle('inactive')
+  if(!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive');
+  }
+
+  aside.classList.add('inactive');
+
 }
