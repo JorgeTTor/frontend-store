49eedf91895a7a6988b29065df9fc8f0cc4030c5
Jorge.T.T <71452849+JorgeTTor@users.noreply.github.com>
2022-08-16 19:40:19 -0500

    [ADD] product card info to index.html

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu')
 const menuHambIcon = document.querySelector('.menu-hamb')
 const mobileMenu = document.querySelector('.mobile-menu')
 const cartIcon = document.querySelector('.navbar-shopping-cart')
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -11,30 +11,30 @@ menuHambIcon.addEventListener('click', toggleMobileMenu);
 cartIcon.addEventListener('click', toggleCartAside);
 
 function toggleDesktopMenu() {
-  const isAsideClosed = aside.classList.contains('inactive');
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
     
   if(!isAsideClosed) {
-      aside.classList.add('inactive');
+      shoppingCartContainer.classList.add('inactive');
     }
   desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-  const isAsideClosed = aside.classList.contains('inactive');
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
   if(!isAsideClosed) {
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
   }
   mobileMenu.classList.toggle('inactive');
 }
 
 function toggleCartAside() {
   const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
-  //aside.classList.toggle('inactive');
+  //shoppingCartContainer.classList.toggle('inactive');
   if(!isMobileMenuClosed) {
     mobileMenu.classList.add('inactive');
   }
-  aside.classList.toggle('inactive');
+  shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList = [];
